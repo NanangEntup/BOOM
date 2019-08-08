d3ab4c3312f4b0e32888c63ac3922285c62bf184
NanangEntup <53732550+NanangEntup@users.noreply.github.com>
2019-08-08 14:42:40 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+clear
+neofetch
+toilet -f standard -F gay INSTALL COK
+pkg install git
+pkg install python
+pkg install python2
+pkg install openssh
+pkg install neofetch
+pkg install bash
+gem install lolcat
+python2 trojanganasenc.py
+figlet CLEAR | lolcat
